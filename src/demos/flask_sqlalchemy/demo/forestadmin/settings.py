085e0b8044eee183b1af966ad768a26eb53b9003
Julien Barreau <julien.barreau@forestadmin.com>
2023-04-28 16:11:14 +0200

    chore(settings): remove useless 'application_url' setting

--- src/demos/flask_sqlalchemy/demo/forestadmin/settings.py
+++ src/demos/flask_sqlalchemy/demo/forestadmin/settings.py
@@ -3,7 +3,5 @@ from forestadmin.agent_toolkit.options import Options
 SETTINGS: Options = {
     "env_secret": "",
     "auth_secret": "",
-    "forest_server_url": "https://api.forestadmin.com",
     "agent_url": "http://127.0.0.1:5000/",
-    "application_url": "http://127.0.0.1:5000/",
 }
