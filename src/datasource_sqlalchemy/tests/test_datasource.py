68a826bee481e3bf4f64b0044d7ad8dc39423e6b
Julien Barreau <julien.barreau@forestadmin.com>
2023-05-05 17:46:22 +0200

    chore(isort): fix import sorting

--- src/datasource_sqlalchemy/tests/test_datasource.py
+++ src/datasource_sqlalchemy/tests/test_datasource.py
@@ -2,7 +2,6 @@ from unittest import TestCase
 from unittest.mock import MagicMock, Mock, patch
 
 from forestadmin.datasource_sqlalchemy.datasource import SqlAlchemyDatasource
-
 from tests.fixture import models
 
 
