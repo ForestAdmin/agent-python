a7ac6a894d1d5687503c2f3135a369c71919b6c2
Julien Barreau <julien.barreau@forestadmin.com>
2023-06-29 14:17:59 +0200

    chore: add test for datasource_decorator

--- src/datasource_toolkit/forestadmin/datasource_toolkit/decorators/datasource_decorator.py
+++ src/datasource_toolkit/forestadmin/datasource_toolkit/decorators/datasource_decorator.py
@@ -13,3 +13,7 @@ class DatasourceDecorator(Datasource):
 
         for collection in self.child_datasource.collections:
             self.add_collection(self.class_collection_decorator(collection, self))
+
+    # def get_charts(self):
+
+    # def render_chart(self, caller:User, name:str):
