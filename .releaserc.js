f389ec0443913e127663678ceef321c21d429ba6
Julien Barreau <julien.barreau@forestadmin.com>
2023-07-21 16:17:55 +0200

    feat(semantic_release): setting up semantic release (#96)

---
+++ .releaserc.js
@@ -0,0 +1,76 @@
+module.exports = {
+  branches: [
+    "main",
+    { name: "beta", channel: "beta", prerelease: true },
+  ],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer', {
+        preset: 'angular',
+        releaseRules: [
+          // Example: `type(scope): subject [force release]`
+          { subject: '*\\[force release\\]*', release: 'patch' },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: 'python .github/update_pyproject_versions.py ${nextRelease.version} ; sed -i \'s/"version": ".*"/"version": "${nextRelease.version}"/g\' package.json;',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: [
+          'CHANGELOG.md',
+          'package.json',
+          'src/agent_toolkit/pyproject.toml',
+          'src/datasource_sqlalchemy/pyproject.toml',
+          'src/datasource_toolkit/pyproject.toml',
+          'src/flask_agent/pyproject.toml'
+        ],
+        message: 'chore(release): ${nextRelease.gitTag} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+    [
+      'semantic-release-slack-bot',
+      {
+        markdownReleaseNotes: true,
+        notifyOnSuccess: true,
+        notifyOnFail: false,
+        onSuccessTemplate: {
+          text: "📦 $package_name@$npm_package_version has been released!",
+          blocks: [{
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: '*New `$package_name` package released!*'
+            }
+          }, {
+            type: 'context',
+            elements: [{
+              type: 'mrkdwn',
+              text: "📦  *Version:* <$repo_url/releases/tag/$npm_package_version|$npm_package_version>"
+            }]
+          }, {
+            type: 'divider',
+          }],
+          attachments: [{
+            blocks: [{
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: '*Changes* of version $release_notes',
+              },
+            }],
+          }],
+        },
+        packageName: 'agent-python',
+      }
+    ],
+  ]
+}
