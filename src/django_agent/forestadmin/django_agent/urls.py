42a82d32cc0c66daade71ab5924a98039eeeaa45
Julien Barreau <julien.barreau@forestadmin.com>
2023-11-20 16:50:39 +0100

    feat(django_agent): handle prefix setting

--- src/django_agent/forestadmin/django_agent/urls.py
+++ src/django_agent/forestadmin/django_agent/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import path
 
 from .views import actions, authentication, charts, crud, crud_related, index, stats
@@ -5,41 +6,49 @@ from .views import actions, authentication, charts, crud, crud_related, index, s
 app_name = "django_agent"
 
 
+prefix = getattr(settings, "FOREST_PREFIX", "")
+if len(prefix) > 0 and prefix[-1] != "/":
+    prefix = f"{prefix}/"
+
 urlpatterns = [
     # generic
-    path("forest/", index.index, name="index"),
-    path("forest/scope-cache-invalidation", index.scope_cache_invalidation, name="scope_invalidation"),
+    path(f"{prefix}forest/", index.index, name="index"),
+    path(f"{prefix}forest/scope-cache-invalidation", index.scope_cache_invalidation, name="scope_invalidation"),
     # authentication
-    path("forest/authentication", authentication.authentication, name="authentication"),
-    path("forest/authentication/callback", authentication.callback, name="authentication_callback"),
+    path(f"{prefix}forest/authentication", authentication.authentication, name="authentication"),
+    path(f"{prefix}forest/authentication/callback", authentication.callback, name="authentication_callback"),
     # actions
-    path("forest/_actions/<int:action_name>/<slug>/hooks/load", actions.hook, name="action_hook_load"),
-    path("forest/_actions/<int:action_name>/<slug>/hooks/change", actions.hook, name="action_hook_change"),
-    path("forest/_actions/<int:action_name>/<slug>", actions.execute, name="action_execute"),
+    path(f"{prefix}forest/_actions/<int:action_name>/<slug>/hooks/load", actions.hook, name="action_hook_load"),
+    path(f"{prefix}forest/_actions/<int:action_name>/<slug>/hooks/change", actions.hook, name="action_hook_change"),
+    path(f"{prefix}forest/_actions/<int:action_name>/<slug>", actions.execute, name="action_execute"),
     # charts
-    path("forest/_charts/<str:chart_name>", charts.chart_datasource, name="datasource_chart"),
-    path("forest/_charts/<str:collection_name>/<str:chart_name>", charts.chart_collection, name="collection_chart"),
+    path(f"{prefix}forest/_charts/<str:chart_name>", charts.chart_datasource, name="datasource_chart"),
+    path(
+        f"{prefix}forest/_charts/<str:collection_name>/<str:chart_name>",
+        charts.chart_collection,
+        name="collection_chart",
+    ),
     # stats
-    path("forest/stats/<str:collection_name>", stats.stats, name="stats"),
+    path(f"{prefix}forest/stats/<str:collection_name>", stats.stats, name="stats"),
     # crud related
     path(
-        "forest/<str:collection_name>/<str:pks>/relationships/<str:relation_name>/count",
+        f"{prefix}forest/<str:collection_name>/<str:pks>/relationships/<str:relation_name>/count",
         crud_related.count,
         name="crud_related_count",
     ),
     path(
-        "forest/<str:collection_name>/<str:pks>/relationships/<str:relation_name>.csv",
+        f"{prefix}forest/<str:collection_name>/<str:pks>/relationships/<str:relation_name>.csv",
         crud_related.csv,
         name="crud_related_csv",
     ),
     path(
-        "forest/<str:collection_name>/<str:pks>/relationships/<str:relation_name>",
+        f"{prefix}forest/<str:collection_name>/<str:pks>/relationships/<str:relation_name>",
         crud_related.list_,
         name="crud_related_list",
     ),
     # crud
-    path("forest/<str:collection_name>.csv", crud.csv, name="crud_csv"),
-    path("forest/<str:collection_name>/count", crud.count, name="crud_count"),
-    path("forest/<str:collection_name>/<str:pks>", crud.detail, name="crud_detail"),
-    path("forest/<str:collection_name>", crud.list_, name="crud_list"),
+    path(f"{prefix}forest/<str:collection_name>.csv", crud.csv, name="crud_csv"),
+    path(f"{prefix}forest/<str:collection_name>/count", crud.count, name="crud_count"),
+    path(f"{prefix}forest/<str:collection_name>/<str:pks>", crud.detail, name="crud_detail"),
+    path(f"{prefix}forest/<str:collection_name>", crud.list_, name="crud_list"),
 ]
