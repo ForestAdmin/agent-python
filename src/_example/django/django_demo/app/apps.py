d2bdfa70f67389abee71038eee58450ec4c84fa9
Julien Barreau <julien.barreau@forestadmin.com>
2023-11-20 18:05:19 +0100

    chore(example): update django example project

--- src/_example/django/django_demo/app/apps.py
+++ src/_example/django/django_demo/app/apps.py
@@ -1,14 +1,6 @@
-from app.forest_admin import customize_forest
-from django.apps import AppConfig, apps
-from forestadmin.django_agent.agent import DjangoAgent
+from django.apps import AppConfig
 
 
 class AppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app"
-
-    def ready(self) -> None:
-        agent: DjangoAgent = apps.get_app_config("django_agent").get_agent()
-        if agent:
-            customize_forest(agent)
-            agent.start()
