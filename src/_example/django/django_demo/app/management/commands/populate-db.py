c6732c44cbcb0be3b747812ecabb350b03e9d874
Julien Barreau <julien.barreau@forestadmin.com>
2023-11-22 14:59:20 +0100

    chore(example): update example project

--- src/_example/django/django_demo/app/management/commands/populate-db.py
+++ src/_example/django/django_demo/app/management/commands/populate-db.py
@@ -1,7 +1,7 @@
 import random
 from datetime import datetime, timezone
 
-from app.models import Address, Cart, Customer, Order
+from app.models import Address, Cart, Customer, CustomerAddress, Order
 from django.contrib.auth.models import Group, User
 from django.core.management.base import BaseCommand
 from faker import Faker
@@ -12,14 +12,46 @@ fake = Faker(["it_IT", "en_US", "ja_JP", "fr_FR"])
 class Command(BaseCommand):
     help = "Create fake data for the database"
 
-    # def add_arguments(self, parser):
-    #     parser.add_argument("poll_ids", nargs="+", type=int)
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-b",
+            "--big-data",
+            help=f"create a lot of data, can take a while(~=5min)). Open this file ({__file__}) to edit the values",
+            action="store_true",
+        )
 
     def handle(self, *args, **options):
-        users, groups = create_users_groups()
-        customers = create_customers()
-        addresses = create_addresses(customers)
-        orders, carts = create_orders_cart(customers, addresses)
+        numbers = {
+            "groups": 4,
+            "users": 10,
+            "customers": 500,
+            "addresses": 500,
+            "orders_carts": 1000,
+        }
+        if options["big_data"]:
+            numbers = {
+                "groups": 50,
+                "users": 1000,
+                "customers": 500000,
+                "addresses": 1000000,
+                "orders_carts": 3000000,
+            }
+
+        users, groups = create_users_groups(numbers["groups"], numbers["users"])
+        if options["verbosity"] != 0:
+            print(f"users({numbers['users']}) and groups({numbers['groups']}) created ")
+
+        customers = create_customers(numbers["customers"])
+        if options["verbosity"] != 0:
+            print(f"customers({numbers['customers']}) created")
+
+        addresses = create_addresses(customers, numbers["addresses"])
+        if options["verbosity"] != 0:
+            print(f"addresses({numbers['addresses']}) created")
+
+        orders, carts = create_orders_cart(customers, addresses, numbers["orders_carts"])
+        if options["verbosity"] != 0:
+            print(f"orders and carts ({numbers['orders_carts']}) created")
 
 
 def create_users_groups(nb_group=4, nb_users=10):
@@ -27,16 +59,26 @@ def create_users_groups(nb_group=4, nb_users=10):
     users = []
     for i in range(nb_group):
         g = Group(name=fake.company())
-        g.save()
-        g.refresh_from_db()
         groups.append(g)
+    Group.objects.bulk_create(groups)
+    groups = Group.objects.all()
 
+    usernames = set()
     for i in range(nb_users):
-        u = User(username=f"{fake.first_name()[0]}{fake.last_name()}")
-        u.save()
-        u.refresh_from_db()
-        u.groups.add(groups[i % (len(groups) - 1)])
+        uname = f"{fake.first_name()[0]}{fake.last_name()}"
+        while uname in usernames:
+            uname = f"{fake.first_name()[0]}{fake.last_name()}"
+        usernames.add(uname)
+        u = User(username=uname)
         users.append(u)
+    User.objects.bulk_create(users)
+    users = User.objects.all()
+
+    groups_users = []
+    for i in range(1, nb_users):
+        groups_users.append(User.groups.through(user_id=i, group_id=(i % nb_group) + 1))
+    User.groups.through.objects.bulk_create(groups_users)
+
     return users, groups
 
 
@@ -44,9 +86,9 @@ def create_customers(nb_customers=500):
     customers = []
     for i in range(nb_customers):
         c = Customer(first_name=fake.first_name(), last_name=fake.last_name(), birthday_date=fake.date_of_birth())
-        c.save()
-        c.refresh_from_db()
         customers.append(c)
+    Customer.objects.bulk_create(customers)
+    customers = Customer.objects.all()
     return customers
 
 
@@ -60,10 +102,16 @@ def create_addresses(customers, nb_addresses=500):
             country=fake.country(),
             zip_code=fake.postcode(),
         )
-        a.save()
-        a.refresh_from_db()
-        a.customers.add(customers[i % (len(customers)) - 1])
         addresses.append(a)
+    Address.objects.bulk_create(addresses)
+    addresses = Address.objects.all()
+
+    customer_addresses = []
+    nb_customer = customers.count()
+    for i in range(1, nb_addresses):
+        customer_addresses.append(CustomerAddress(address_id=i, customer_id=((i - 1) % nb_customer) + 1))
+    CustomerAddress.objects.bulk_create(customer_addresses)
+
     return addresses
 
 
@@ -71,8 +119,9 @@ def create_orders_cart(customers, addresses, nb_order=1000):
     orders = []
     carts = []
 
-    for i in range(nb_order):
+    for i in range(1, nb_order + 1):
         o = Order(
+            id=i,
             ordered_at=fake.date_time_between(start_date=datetime(2015, 1, 1), tzinfo=timezone.utc),
             amount=random.randint(0, 100000) / 100,
             customer=customers[i % (len(customers) - 1)],
@@ -80,12 +129,13 @@ def create_orders_cart(customers, addresses, nb_order=1000):
             delivering_address=addresses[i % (len(addresses) - 1)],
             status=random.choice(list(Order.OrderStatus)),
         )
-        o.save()
-        o.refresh_from_db()
         orders.append(o)
+    Order.objects.bulk_create(orders)
+    orders = Order.objects.all()
 
-        c = Cart(name=fake.city(), order=o)
-        c.save()
-        c.refresh_from_db()
+    for i in range(1, nb_order + 1):
+        c = Cart(name=fake.city(), order_id=i)
         carts.append(c)
+    Cart.objects.bulk_create(carts)
+    carts = Cart.objects.all()
     return orders, carts
