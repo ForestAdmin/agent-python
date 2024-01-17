ed629acef66a5a88ab5ed9df4cee3ea45301b250
Julien Barreau <julien.barreau@forestadmin.com>
2024-01-17 14:33:32 +0100

    fix(django_setup): wait for all apps to be initialized before creating agent

--- src/django_agent/forestadmin/django_agent/apps.py
+++ src/django_agent/forestadmin/django_agent/apps.py
@@ -1,9 +1,10 @@
 import asyncio
 import importlib
 import sys
+import threading
 from typing import Callable, Optional, Union
 
-from django.apps import AppConfig
+from django.apps import AppConfig, apps
 from django.conf import settings
 from forestadmin.agent_toolkit.forest_logger import ForestLogger
 from forestadmin.datasource_django.datasource import DjangoDatasource
@@ -59,6 +60,7 @@ def _call_user_customize_function(customize_fn: Union[str, Callable[[DjangoAgent
 class DjangoAgentApp(AppConfig):
     _DJANGO_AGENT: DjangoAgent = None
     name = "forestadmin.django_agent"
+    _waiter_timeout = 5.0  # seconds
 
     @classmethod
     def get_agent(cls) -> DjangoAgent:
@@ -73,4 +75,27 @@ class DjangoAgentApp(AppConfig):
         return cls._DJANGO_AGENT
 
     def ready(self):
+        # we need to wait for other apps to be ready, for this forest app must be ready
+        # that's why we need another thread waiting for every app to be ready
+        t = threading.Thread(name="forest.wait_and_launch_agent", target=self._wait_for_all_apps_ready_and_launch_agent)
+        t.start()
+
+    def _wait_for_all_apps_ready_and_launch_agent(self):
+        wait_delay = 0.1
+        nb_retry = int(self._waiter_timeout / wait_delay)
+
+        wait_retry_count = 0
+        django_ready = apps.ready_event.wait(wait_delay)
+
+        while django_ready is False and wait_retry_count < nb_retry:
+            wait_retry_count += 1
+            django_ready = apps.ready_event.wait(wait_delay)
+
+        if django_ready is False:
+            ForestLogger.log(
+                "warning",
+                f"Django 'apps.ready_event' never set after {wait_delay*nb_retry:.2f} seconds. "
+                "Trying to launch agent anyway.",
+            )
+
         DjangoAgentApp._DJANGO_AGENT = init_app_agent()
