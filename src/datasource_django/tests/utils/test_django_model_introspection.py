6d2792957778e9a43a5596cf3aa2368062759156
Julien Barreau <julien.barreau@forestadmin.com>
2024-01-29 10:26:05 +0100

    feat(django_models_inheritance): support django model inheritance (#176)

--- src/datasource_django/tests/utils/test_django_model_introspection.py
+++ src/datasource_django/tests/utils/test_django_model_introspection.py
@@ -13,7 +13,7 @@ class TestDjangoFieldFactory(TestCase):
     def test_build_should_build_string_field(self):
         """basics"""
         field = models.TextField(null=True)
-        field_schema = FieldFactory.build(field)
+        field_schema = FieldFactory.build(field, None)
         self.assertEqual(field_schema["column_type"], PrimitiveType.STRING)
         self.assertEqual(field_schema["is_primary_key"], False)
         self.assertEqual(field_schema["is_read_only"], False)
@@ -25,19 +25,20 @@ class TestDjangoFieldFactory(TestCase):
     def test_build_should_build_with_default_value(self):
         """default values"""
         field = models.TextField(null=True, default="my default value")
-        field_schema = FieldFactory.build(field)
+        field_schema = FieldFactory.build(field, None)
         self.assertEqual(field_schema["default_value"], "my default value")
 
     def test_build_should_ignore_dynamic_default_value(self):
         """default values"""
         field = models.TextField(null=True, default=lambda: "my default value")
-        field_schema = FieldFactory.build(field)
+        field_schema = FieldFactory.build(field, None)
         self.assertEqual(field_schema["default_value"], None)
 
     def test_build_should_handle_primary_key(self):
         """primary key"""
         field = models.IntegerField(primary_key=True)
-        field_schema = FieldFactory.build(field)
+        field.model = None
+        field_schema = FieldFactory.build(field, None)
         self.assertEqual(field_schema["is_primary_key"], True)
 
     def test_build_should_handle_enums(self):
@@ -48,7 +49,7 @@ class TestDjangoFieldFactory(TestCase):
             (3, "BOTTOM"),
         ]
         field = models.IntegerField(choices=choices)
-        field_schema = FieldFactory.build(field)
+        field_schema = FieldFactory.build(field, None)
 
         self.assertEqual(field_schema["enum_values"], ["1", "2", "3"])
         self.assertEqual(field_schema["column_type"], PrimitiveType.ENUM)
@@ -62,7 +63,7 @@ class TestDjangoFieldFactory(TestCase):
             (now, "NOW"),
         ]
         field = models.DateField(choices=choices)
-        field_schema = FieldFactory.build(field)
+        field_schema = FieldFactory.build(field, None)
 
         self.assertEqual(field_schema["enum_values"], ["1916-01-01 01:01:01", "1941-02-02 02:02:02", str(now)])
         self.assertEqual(field_schema["column_type"], PrimitiveType.ENUM)
@@ -70,42 +71,43 @@ class TestDjangoFieldFactory(TestCase):
     def test_build_should_handle_read_only_for_auto_increment_pk(self):
         """readonly"""
         field = models.AutoField(primary_key=True)
-        field_schema = FieldFactory.build(field)
+        field.model = None
+        field_schema = FieldFactory.build(field, None)
 
         self.assertEqual(field_schema["is_read_only"], True)
 
     def test_build_should_handle_read_only_for_date_auto_now(self):
         """readonly"""
         field = models.DateField(auto_now=True)
-        field_schema = FieldFactory.build(field)
+        field_schema = FieldFactory.build(field, None)
 
         self.assertEqual(field_schema["is_read_only"], True)
 
     def test_build_should_handle_read_only_field_is_editable(self):
         """readonly"""
         field = models.CharField(max_length=254, editable=False)
-        field_schema = FieldFactory.build(field)
+        field_schema = FieldFactory.build(field, None)
 
         self.assertEqual(field_schema["is_read_only"], True)
 
     def test_build_should_handle_read_only_field_is_not_editable(self):
         """readonly"""
         field = models.CharField(max_length=254, editable=True)
-        field_schema = FieldFactory.build(field)
+        field_schema = FieldFactory.build(field, None)
 
         self.assertEqual(field_schema["is_read_only"], False)
 
     def test_build_should_handle_present_validator(self):
         """validator"""
         field = models.DateField()
-        field_schema = FieldFactory.build(field)
+        field_schema = FieldFactory.build(field, None)
 
         self.assertEqual(field_schema["validations"], [{"operator": Operator.PRESENT}])
 
     def test_build_should_handle_max_len_validator_for_max_length_attr(self):
         """validator"""
         field = models.CharField(max_length=254)
-        field_schema = FieldFactory.build(field)
+        field_schema = FieldFactory.build(field, None)
 
         self.assertEqual(
             field_schema["validations"],
@@ -144,7 +146,9 @@ class TestDjangoCollectionFactory(TestCase):
         ) as spy_field_factory_build:
             schema = DjangoCollectionFactory.build(self.field_only_model)
             for name, field_schema in schema["fields"].items():
-                spy_field_factory_build.assert_any_call(self.field_only_model._meta.get_field(name))
+                spy_field_factory_build.assert_any_call(
+                    self.field_only_model._meta.get_field(name), self.field_only_model
+                )
         self.assertEqual(set(schema["fields"].keys()), set(["id", "name"]))
 
     def test_build_should_handle_one_to_one_relations(self):
