b02e0fde8878568d16020c859aa2ee3258a59043
Julien Barreau <julien.barreau@forestadmin.com>
2024-05-03 11:47:24 +0200

    chore: update example project

--- src/_example/fastapi/src/main.py
+++ src/_example/fastapi/src/main.py
@@ -1,66 +1,41 @@
 import logging
 import os
-from contextlib import asynccontextmanager
-from typing import Any, Dict
 
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from forestadmin.datasource_sqlalchemy.datasource import SqlAlchemyDatasource
 from forestadmin.fastapi_agent.agent import create_agent
-from sqlalchemy import select
 from src.config import settings
-from src.database import Base, get_session
-from src.with_sql_alchemy.models import Address, Customer
+from src.custom_logs import ColorizedFormatter
+from src.with_sql_alchemy.models import Base
 
-app = FastAPI(
-    debug=settings.debug,
-    # lifespan=lifespan,
-)
+forest_logger = logging.getLogger("forestadmin")
+
+app = FastAPI(debug=settings.debug)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origin_regex=r"https:\/\/.*\.forestadmin.com",
     allow_credentials=True,
-    allow_methods="*",
-    allow_headers="*",
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["authorization"],
 )
 
 agent = create_agent(app, settings.model_dump())
 agent.add_datasource(SqlAlchemyDatasource(Base, settings.db_uri))
 
+forest_logger.setLevel(logging.DEBUG)
 
-COLLECTION_NAME_TO_MODEL: Dict[str, Any] = {
-    "Address": Address,
-    "Customer": Customer,
-}
-
-
-# @app.get("/")
-# async def root():
-#     with get_session()().begin() as session:
-#         items = session.session.scalars(select(Address).limit(15))
-#         return [i.__dict__ for i in items]
-
-
-@app.get(
-    "/a/{collection}/",
-)
-async def list_collection(collection: str):
-    with get_session()().begin() as session:
-        items = session.session.scalars(select(Address))
-        return [i.__dict__ for i in items]
-    # async with aget_session().begin() as session:
-    #     items = await session.scalars(select(COLLECTION_NAME_TO_MODEL[collection]))
-    #     items = [i.__dict__ for i in items]
-    #     for i in items:
-    #         del i["_sa_instance_state"]
-    #     return [i for i in items]
+for h in forest_logger.handlers:
+    h.setLevel(logging.DEBUG)
+    h.setFormatter(ColorizedFormatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"))
 
 
 if __name__ == "__main__":
     print(os.environ.get("FASTAPI_APP", "main:app"))
     uvicorn.run(
-        app,  # os.environ.get("FASTAPI_APP", "main:app"),
+        app=app,  # os.environ.get("FASTAPI_APP", "main:app"),
         host=os.environ.get("FASTPI_HOST", "0.0.0.0"),
         port=int(os.environ.get("FASTPI_PORT", "8000")),
     )
