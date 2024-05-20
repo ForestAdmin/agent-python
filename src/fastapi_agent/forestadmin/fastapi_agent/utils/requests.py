7ee8def3143317644ca9fbe0d7713be7bde383a8
Julien Barreau <julien.barreau@forestadmin.com>
2024-05-20 09:30:43 +0200

    fix: update request and response transsformation

--- src/fastapi_agent/forestadmin/fastapi_agent/utils/requests.py
+++ src/fastapi_agent/forestadmin/fastapi_agent/utils/requests.py
@@ -15,7 +15,7 @@ HTTP_METHOD_MAPPING = {
 async def convert_request(fastapi_request: FastAPIRequest):
     method = HTTP_METHOD_MAPPING[fastapi_request.method]
     query = {**fastapi_request.path_params}
-    query.update(fastapi_request.query_params)
+    query.update(**fastapi_request.query_params)
     kwargs: Dict[str, Any] = {"query": query, "headers": {k.title(): v for k, v in fastapi_request.headers.items()}}
 
     if method in [RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE] and await fastapi_request.body():
@@ -27,14 +27,15 @@ async def convert_request(fastapi_request: FastAPIRequest):
 
 def convert_response(response: Union[Response, FileResponse]) -> FastAPIResponse:
     if isinstance(response, FileResponse):
-        flask_response = FastAPIResponse(
-            response.file,
+        content = response.file.read() if response.file is not None else None
+        fastapi_response = FastAPIResponse(
+            content,
             headers={
                 "Content-Type": response.mimetype,
-                "Content-Disposition": f"attachment; filename={response.name}",
+                "Content-Disposition": f'attachment; filename="{response.name}"',
                 **response.headers,
             },
         )
     else:
-        flask_response = FastAPIResponse(response.body, response.status, headers={**response.headers})
-    return flask_response
+        fastapi_response = FastAPIResponse(response.body, response.status, headers={**response.headers})
+    return fastapi_response
