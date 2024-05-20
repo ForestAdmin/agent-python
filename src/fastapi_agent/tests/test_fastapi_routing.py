ae196733467566612cd8e764c2bd4e600e7955fc
Julien Barreau <julien.barreau@forestadmin.com>
2024-05-20 10:12:44 +0200

    chore: add tests

---
+++ src/fastapi_agent/tests/test_fastapi_routing.py
@@ -0,0 +1,259 @@
+import asyncio
+from unittest import TestCase
+from unittest.mock import ANY, AsyncMock, patch
+
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from forestadmin.agent_toolkit.utils.context import Response
+from forestadmin.fastapi_agent.agent import create_agent
+
+
+class TestFastApiRouting(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.loop = asyncio.new_event_loop()
+        cls.mocked_resources = {}
+        for key in [
+            "authentication",
+            "crud",
+            "crud_related",
+            "stats",
+            "actions",
+            "collection_charts",
+            "datasource_charts",
+        ]:
+            cls.mocked_resources[key] = AsyncMock()
+            cls.mocked_resources[key].dispatch = AsyncMock(
+                return_value=Response(200, '{"mock": "ok"}', headers={"content-type": "application/json"})
+            )
+
+        cls.app = FastAPI()
+        patch(
+            "forestadmin.agent_toolkit.agent.Agent.get_resources",
+            return_value=cls.mocked_resources,
+            new_callable=AsyncMock,
+        ).start()
+
+        cls.agent = create_agent(
+            cls.app,
+            {
+                "FOREST_ENV_SECRET": "da4fc9331a68a18c2262154c74d9acb22f335724c8f2a510f8df187fa808703e",
+                "FOREST_AUTH_SECRET": "fake",
+            },
+        )
+        cls.client = TestClient(cls.app)
+
+
+class TestFastApiRoutingWelcome(TestFastApiRouting):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.stats_dispatch_mock: AsyncMock = cls.loop.run_until_complete(cls.agent.get_resources())["stats"].dispatch
+        cls.collection_charts_dispatch_mock: AsyncMock = cls.loop.run_until_complete(cls.agent.get_resources())[
+            "collection_charts"
+        ].dispatch
+        cls.datasource_charts_dispatch_mock: AsyncMock = cls.loop.run_until_complete(cls.agent.get_resources())[
+            "datasource_charts"
+        ].dispatch
+
+    def test_index(self):
+        response = self.client.get("/forest")
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.content, b"")
+
+    def test_stats(self):
+        response = self.client.post("/forest/stats/customer")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.stats_dispatch_mock.assert_any_call(ANY)
+
+    def test_collection_chart_get(self):
+        response = self.client.get("/forest/_charts/customer/test_chart?timezone=Europe%2FParis")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.collection_charts_dispatch_mock.assert_called_with(ANY)
+
+    def test_collection_chart_post(self):
+        response = self.client.post("/forest/_charts/customer/test_chart?timezone=Europe%2FParis")
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.assertEqual(response.status_code, 200)
+        self.collection_charts_dispatch_mock.assert_called_with(ANY)
+
+    def test_datasource_chart_get(self):
+        response = self.client.get("/forest/_charts/test_chart?timezone=Europe%2FParis")
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.assertEqual(response.status_code, 200)
+        self.datasource_charts_dispatch_mock.assert_called_with(ANY)
+
+    def test_datasource_chart_post(self):
+        response = self.client.post("/forest/_charts/test_chart?timezone=Europe%2FParis")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.datasource_charts_dispatch_mock.assert_called_with(ANY)
+
+        with patch.object(self.agent._permission_service, "invalidate_cache") as mocked_invalidate_cache:
+            response = self.client.post("/forest/scope-cache-invalidation")
+            mocked_invalidate_cache.assert_called_with("forest.scopes")
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.content, b"")
+
+
+class TestFastApiRoutingAction(TestFastApiRouting):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.action_dispatch_mock: AsyncMock = cls.loop.run_until_complete(cls.agent.get_resources())["actions"].dispatch
+
+    def test_hook_load(self):
+        response = self.client.post("/forest/_actions/customer/1/action_name/hooks/load")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.action_dispatch_mock.assert_any_call(ANY, "hook")
+
+    def test_hook_change(self):
+        response = self.client.post("/forest/_actions/customer/1/action_name/hooks/change")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.action_dispatch_mock.assert_any_call(ANY, "hook")
+
+    def test_hook_search(self):
+        response = self.client.post("/forest/_actions/customer/1/action_name/hooks/search")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.action_dispatch_mock.assert_any_call(ANY, "hook")
+
+    def test_action(self):
+        response = self.client.post("/forest/_actions/customer/1/action_name")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.action_dispatch_mock.assert_any_call(ANY, "execute")
+
+
+class TestFastApiRoutingAuthentication(TestFastApiRouting):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.authentication_dispatch_mock: AsyncMock = cls.loop.run_until_complete(cls.agent.get_resources())[
+            "authentication"
+        ].dispatch
+
+    def test_auth(self):
+        response = self.client.post("/forest/authentication")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.authentication_dispatch_mock.assert_any_call(ANY, "authenticate")
+
+    def test_auth_callback(self):
+        response = self.client.get("/forest/authentication/callback")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.authentication_dispatch_mock.assert_any_call(ANY, "callback")
+
+
+class TestFastApiRoutingCRUD(TestFastApiRouting):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.crud_dispatch_mock: AsyncMock = cls.loop.run_until_complete(cls.agent.get_resources())["crud"].dispatch
+
+    def test_list_list(self):
+        response = self.client.get("/forest/customer?timezone=Europe%2FParis")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.crud_dispatch_mock.assert_called_with(ANY, "list")
+
+    def test_list_create(self):
+        response = self.client.post("/forest/customer?timezone=Europe%2FParis", json={})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.crud_dispatch_mock.assert_called_with(ANY, "add")
+
+    def test_list_del(self):
+        response = self.client.delete("/forest/customer?timezone=Europe%2FParis")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.crud_dispatch_mock.assert_called_with(ANY, "delete_list")
+
+    def test_csv(self):
+        response = self.client.get("/forest/customer.csv?timezone=Europe%2FParis")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.crud_dispatch_mock.assert_called_with(ANY, "csv")
+
+    def test_count(self):
+        response = self.client.get("/forest/customer/count?timezone=Europe%2FParis")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.crud_dispatch_mock.assert_called_with(ANY, "count")
+
+    def test_detail_edit(self):
+        response = self.client.put("/forest/customer/1?timezone=Europe%2FParis", json={})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.crud_dispatch_mock.assert_called_with(ANY, "update")
+
+    def test_detail_get(self):
+        response = self.client.get("/forest/customer/1?timezone=Europe%2FParis")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.crud_dispatch_mock.assert_called_with(ANY, "get")
+
+    def test_detail_del(self):
+        response = self.client.delete("/forest/customer/1?timezone=Europe%2FParis")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.crud_dispatch_mock.assert_called_with(ANY, "delete")
+
+
+class TestFastApiRoutingCRUDRelated(TestFastApiRouting):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.crud_related_dispatch_mock: AsyncMock = cls.loop.run_until_complete(cls.agent.get_resources())[
+            "crud_related"
+        ].dispatch
+
+    def test_list_related_list(self):
+        response = self.client.get("/forest/customer/1/relationships/orders?timezone=Europe%2FParis")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.crud_related_dispatch_mock.assert_called_with(ANY, "list")
+
+    def test_list_related_add(self):
+        # attach / add
+        response = self.client.post("/forest/customer/1/relationships/orders?timezone=Europe%2FParis", data={})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.crud_related_dispatch_mock.assert_called_with(ANY, "add")
+
+    def test_list_related_delete_list(self):
+        # detach
+        response = self.client.delete("/forest/customer/1/relationships/orders?timezone=Europe%2FParis")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.crud_related_dispatch_mock.assert_called_with(ANY, "delete_list")
+
+        # delete related relation item
+        response = self.client.delete("/forest/customer/1/relationships/orders?timezone=Europe%2FParis&delete=True")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.crud_related_dispatch_mock.assert_called_with(ANY, "delete_list")
+
+    def test_list_related_update_list(self):
+        # ??
+        response = self.client.put("/forest/customer/1/relationships/orders?timezone=Europe%2FParis")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.crud_related_dispatch_mock.assert_called_with(ANY, "update_list")
+
+    def test_count_related(self):
+        response = self.client.get("/forest/customer/1/relationships/orders/count?timezone=Europe%2FParis")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.crud_related_dispatch_mock.assert_called_with(ANY, "count")
+
+    def test_csv_related(self):
+        response = self.client.get("/forest/customer/1/relationships/orders.csv?timezone=Europe%2FParis")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"mock": "ok"})
+        self.crud_related_dispatch_mock.assert_called_with(ANY, "csv")
--- src/fastapi_agent/tests/utils/test_requests.py
+++ src/fastapi_agent/tests/utils/test_requests.py
@@ -1,68 +1,76 @@
+import asyncio
 from io import BytesIO
 from unittest import TestCase
-from unittest.mock import Mock
+from unittest.mock import AsyncMock, Mock
 
-from flask.wrappers import Request as FlaskRequest
-from flask.wrappers import Response as FlaskResponse
+from fastapi import Request as FastAPIRequest
+from fastapi import Response as FastAPIResponse
 from forestadmin.agent_toolkit.utils.context import FileResponse, Request, Response
-from forestadmin.flask_agent.utils.requests import convert_request, convert_response
+from forestadmin.fastapi_agent.utils.requests import convert_request, convert_response
 
 
 class TestRequests(TestCase):
-    def test_convert_request_get(self):
-        flask_request_mock = Mock(FlaskRequest)
-        flask_request_mock.method = "GET"
-        flask_request_mock.args = {}
-        flask_request_mock.view_args = {"collection_name": "customer"}
-        flask_request_mock.headers = {"host": "127.0.0.1:5000", "host_url": "http://127.0.0.1:5000/"}
-        flask_request_mock.remote_addr = "127.0.0.1"
-        request = convert_request(flask_request_mock)
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.loop = asyncio.new_event_loop()
 
-        assert isinstance(request, Request)
-        assert request.method.name == flask_request_mock.method
-        assert request.body is None
-        assert request.query == flask_request_mock.view_args
-        assert request.headers == flask_request_mock.headers
-        assert request.user is None
-        assert request.client_ip == "127.0.0.1"
+    def test_convert_request_get(self):
+        flask_request_mock = Mock(FastAPIRequest)
+        flask_request_mock.method = "GET"
+        flask_request_mock.query_params = {}
+        flask_request_mock.path_params = {"collection_name": "customer"}
+        flask_request_mock.headers = {"host": "127.0.0.1:5000", "host_url": "http://127.0.0.1:5000/"}
+        flask_request_mock.client = Mock()
+        flask_request_mock.client.host = "127.0.0.1"
+        request = self.loop.run_until_complete(convert_request(flask_request_mock))
+
+        self.assertTrue(isinstance(request, Request))
+        self.assertEqual(request.method.name, flask_request_mock.method)
+        self.assertEqual(request.body, None)
+        self.assertEqual(request.query, flask_request_mock.path_params)
+        self.assertEqual(request.headers, {"Host": "127.0.0.1:5000", "Host_Url": "http://127.0.0.1:5000/"})
+        self.assertEqual(request.user, None)
+        self.assertEqual(request.client_ip, "127.0.0.1")
 
     def test_convert_request_post(self):
-        flask_request_mock = Mock(FlaskResponse)
+        flask_request_mock = Mock(FastAPIResponse)
         flask_request_mock.method = "POST"
-        flask_request_mock.view_args = {"collection_name": "customer"}
-        flask_request_mock.args = {}
-        flask_request_mock.view_args = {}
+        flask_request_mock.path_params = {"collection_name": "customer"}
+        flask_request_mock.query_params = {}
+        flask_request_mock.path_params = {}
         flask_request_mock.headers = {"host": "127.0.0.1:5000", "host_url": "http://127.0.0.1:5000/"}
-        flask_request_mock.remote_addr = "127.0.0.1"
+        flask_request_mock.client = Mock()
+        flask_request_mock.client.host = "127.0.0.1"
 
-        flask_request_mock.get_data = Mock(return_value=b'{"renderingId":"9"}')
-        flask_request_mock.json = {"renderingId": "9"}
+        flask_request_mock.body = AsyncMock(return_value=b'{"renderingId":"9"}')
+        flask_request_mock.json = AsyncMock(return_value={"renderingId": "9"})
 
-        request = convert_request(flask_request_mock)
+        request = self.loop.run_until_complete(convert_request(flask_request_mock))
 
-        assert isinstance(request, Request)
-        assert request.method.name == flask_request_mock.method
-        assert request.body == flask_request_mock.json
-        assert request.query == flask_request_mock.view_args
-        assert request.headers == flask_request_mock.headers
-        assert request.user is None
-        assert request.client_ip == "127.0.0.1"
+        self.assertTrue(isinstance(request, Request))
+        self.assertEqual(request.method.name, flask_request_mock.method)
+        self.assertEqual(request.body, {"renderingId": "9"})
+        self.assertEqual(request.query, flask_request_mock.path_params)
+        self.assertEqual(request.headers, {"Host": "127.0.0.1:5000", "Host_Url": "http://127.0.0.1:5000/"})
+        self.assertEqual(request.user, None)
+        self.assertEqual(request.client_ip, "127.0.0.1")
 
     def test_client_ip_must_be_the_forwarded_one(self):
-        flask_request_mock = Mock(FlaskRequest)
+        flask_request_mock = Mock(FastAPIRequest)
         flask_request_mock.method = "GET"
-        flask_request_mock.args = {}
-        flask_request_mock.view_args = {"collection_name": "customer"}
+        flask_request_mock.query_params = {}
+        flask_request_mock.path_params = {"collection_name": "customer"}
         flask_request_mock.headers = {
             "host": "127.0.0.1:5000",
             "host_url": "http://127.0.0.1:5000/",
-            "X-Forwarded-For": "192.168.1.10",
+            "x-forwarded-for": "192.168.1.10",
         }
-        flask_request_mock.remote_addr = "127.0.0.1"
-        request = convert_request(flask_request_mock)
+        flask_request_mock.client = Mock()
+        flask_request_mock.client.host = "127.0.0.1"
+        request = self.loop.run_until_complete(convert_request(flask_request_mock))
 
-        assert request.client_ip != flask_request_mock.remote_addr
-        assert request.client_ip == flask_request_mock.headers.get("X-Forwarded-For")
+        self.assertNotEqual(request.client_ip, flask_request_mock.client.host)
+        self.assertEqual(request.client_ip, "192.168.1.10")
 
 
 class TestResponse(TestCase):
@@ -71,12 +79,12 @@ class TestResponse(TestCase):
         mocked_response.body = '{"data": []}'
         mocked_response.status = 200
         mocked_response.headers = {"Content-Type": "application/json"}
-        response = convert_response(mocked_response)
+        fastapi_response = convert_response(mocked_response)
 
-        assert isinstance(response, FlaskResponse)
-        assert response.status_code == mocked_response.status
-        assert response.status == "200 OK"
-        assert response.headers["Content-Type"] == "application/json"
+        self.assertTrue(isinstance(fastapi_response, FastAPIResponse))
+        self.assertEqual(fastapi_response.status_code, mocked_response.status)
+        self.assertEqual(fastapi_response.headers["Content-Type"], "application/json")
+        self.assertEqual(fastapi_response.body, mocked_response.body.encode("utf-8"))
 
     def test_file_response(self):
         mocked_file_response = Mock(FileResponse)
@@ -84,15 +92,12 @@ class TestResponse(TestCase):
         mocked_file_response.mimetype = "application/json"
         mocked_file_response.name = "test.json"
         mocked_file_response.file = BytesIO('{"data": []}'.encode("utf-8"))
-        mocked_file_response.headers = {}
+        mocked_file_response.headers = {"custom-header": "custom-value"}
         response = convert_response(mocked_file_response)
 
-        assert isinstance(response, FlaskResponse)
-        assert response.status_code == mocked_file_response.status
-        assert response.status == "200 OK"
-        assert response.headers["Content-Type"] == "application/json"
-        assert response.content_type == "application/json"
-        assert response.mimetype == "application/json"
-        assert response.headers["Content-Disposition"] == "attachment; filename=test.json"
-        assert response.headers["Content-Disposition"] == "attachment; filename=test.json"
-        assert response.data == b'{"data": []}'
+        self.assertTrue(isinstance(response, FastAPIResponse))
+        self.assertEqual(response.status_code, mocked_file_response.status)
+        self.assertEqual(response.headers["custom-header"], "custom-value")
+        self.assertEqual(response.headers["content-type"], "application/json")
+        self.assertEqual(response.headers["content-disposition"], 'attachment; filename="test.json"')
+        self.assertEqual(response.body, b'{"data": []}')
