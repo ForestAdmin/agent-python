d4f84cc2765672107193d1fbd25414cfb38401f8
Julien Barreau <julien.barreau@forestadmin.com>
2024-12-10 10:22:19 +0100

    chore: add tests

--- src/datasource_toolkit/tests/decorators/lazy_join/test_lazy_join_decorator.py
+++ src/datasource_toolkit/tests/decorators/lazy_join/test_lazy_join_decorator.py
@@ -13,9 +13,9 @@ from forestadmin.datasource_toolkit.collections import Collection
 from forestadmin.datasource_toolkit.datasources import Datasource
 from forestadmin.datasource_toolkit.decorators.datasource_decorator import DatasourceDecorator
 from forestadmin.datasource_toolkit.decorators.lazy_join.collection import LazyJoinCollectionDecorator
-from forestadmin.datasource_toolkit.interfaces.fields import Column, FieldType, ManyToOne, OneToOne, PrimitiveType
+from forestadmin.datasource_toolkit.interfaces.fields import Column, FieldType, ManyToOne, OneToMany, PrimitiveType
 from forestadmin.datasource_toolkit.interfaces.query.condition_tree.nodes.leaf import ConditionTreeLeaf
-from forestadmin.datasource_toolkit.interfaces.query.filter.unpaginated import Filter
+from forestadmin.datasource_toolkit.interfaces.query.filter.paginated import PaginatedFilter
 from forestadmin.datasource_toolkit.interfaces.query.projections import Projection
 
 
@@ -61,7 +61,7 @@ class TestEmptyCollectionDecorator(TestCase):
                 "id": Column(column_type=PrimitiveType.NUMBER, is_primary_key=True, type=FieldType.COLUMN),
                 "first_name": Column(column_type=PrimitiveType.STRING, type=FieldType.COLUMN),
                 "last_name": Column(column_type=PrimitiveType.STRING, type=FieldType.COLUMN),
-                "book": OneToOne(origin_key="author_id", origin_key_target="id", foreign_collection="Book"),
+                "books": OneToMany(origin_key="author_id", origin_key_target="id", foreign_collection="Book"),
             }
         )
 
@@ -72,70 +72,156 @@ class TestEmptyCollectionDecorator(TestCase):
 
     def test_should_not_join_when_projection_ask_for_target_field_only(self):
         with patch.object(
-            self.collection_book, "list", new_callable=AsyncMock, return_value=[{"id": 1, "author_id": 2}]
+            self.collection_book,
+            "list",
+            new_callable=AsyncMock,
+            return_value=[{"id": 1, "author_id": 2}, {"id": 2, "author_id": 5}],
         ) as mock_list:
             result = self.loop.run_until_complete(
                 self.decorated_book_collection.list(
                     self.mocked_caller,
-                    Filter({}),
+                    PaginatedFilter({}),
                     Projection("id", "author:id"),
                 )
             )
-            mock_list.assert_awaited_once_with(self.mocked_caller, Filter({}), Projection("id", "author_id"))
+            mock_list.assert_awaited_once_with(self.mocked_caller, PaginatedFilter({}), Projection("id", "author_id"))
 
         # should contain author object, without author_id FK
-        self.assertEqual([{"id": 1, "author": {"id": 2}}], result)
+        self.assertEqual([{"id": 1, "author": {"id": 2}}, {"id": 2, "author": {"id": 5}}], result)
 
-    def test_should_join_when_projection_ask_for_multiple_fields_in_relation(self):
+    def test_should_join_when_projection_ask_for_multiple_fields_in_foreign_collection(self):
         with patch.object(
             self.collection_book,
             "list",
             new_callable=AsyncMock,
-            return_value=[{"id": 1, "author": {"id": 2, "first_name": "Isaac"}}],
+            return_value=[
+                {"id": 1, "author": {"id": 2, "first_name": "Isaac"}},
+                {"id": 2, "author": {"id": 5, "first_name": "J.K."}},
+            ],
         ) as mock_list:
             result = self.loop.run_until_complete(
                 self.decorated_book_collection.list(
                     self.mocked_caller,
-                    Filter({}),
+                    PaginatedFilter({}),
                     Projection("id", "author:id", "author:first_name"),
                 )
             )
             mock_list.assert_awaited_once_with(
-                self.mocked_caller, Filter({}), Projection("id", "author:id", "author:first_name")
+                self.mocked_caller, PaginatedFilter({}), Projection("id", "author:id", "author:first_name")
             )
 
-        self.assertEqual([{"id": 1, "author": {"id": 2, "first_name": "Isaac"}}], result)
+        self.assertEqual(
+            [
+                {"id": 1, "author": {"id": 2, "first_name": "Isaac"}},
+                {"id": 2, "author": {"id": 5, "first_name": "J.K."}},
+            ],
+            result,
+        )
 
-    def test_should_replace_in_condition_tree(self):
+    def test_should_not_join_when_condition_tree_is_on_foreign_key_target(self):
         with patch.object(
-            self.collection_book, "list", new_callable=AsyncMock, return_value=[{"id": 1, "author_id": 2}]
+            self.collection_book,
+            "list",
+            new_callable=AsyncMock,
+            return_value=[{"id": 1, "author_id": 2}, {"id": 2, "author_id": 5}, {"id": 3, "author_id": 5}],
         ) as mock_list:
             self.loop.run_until_complete(
                 self.decorated_book_collection.list(
                     self.mocked_caller,
-                    Filter({"condition_tree": ConditionTreeLeaf("author:id", "equal", 2)}),
+                    PaginatedFilter({"condition_tree": ConditionTreeLeaf("author:id", "in", [2, 5])}),
                     Projection("id", "author:id"),
                 )
             )
             mock_list.assert_awaited_once_with(
                 self.mocked_caller,
-                Filter({"condition_tree": ConditionTreeLeaf("author_id", "equal", 2)}),
+                PaginatedFilter({"condition_tree": ConditionTreeLeaf("author_id", "in", [2, 5])}),
                 Projection("id", "author_id"),
             )
 
-    def test_should_not_replace_in_condition_tree(self):
+    def test_should_join_when_condition_tree_is_on_foreign_collection_fields(self):
         with patch.object(
-            self.collection_book, "list", new_callable=AsyncMock, return_value=[{"id": 1, "author_id": 2}]
+            self.collection_book,
+            "list",
+            new_callable=AsyncMock,
+            return_value=[{"id": 1, "author_id": 2}, {"id": 2, "author_id": 5}, {"id": 3, "author_id": 5}],
         ) as mock_list:
             self.loop.run_until_complete(
                 self.decorated_book_collection.list(
                     self.mocked_caller,
-                    Filter({"condition_tree": ConditionTreeLeaf("author:first_name", "equal", "Isaac")}),
+                    PaginatedFilter(
+                        {"condition_tree": ConditionTreeLeaf("author:first_name", "in", ["Isaac", "J.K."])}
+                    ),
                     Projection("id", "author:id"),
                 )
             )
             mock_list.assert_awaited_once_with(
                 self.mocked_caller,
-                Filter({"condition_tree": ConditionTreeLeaf("author:first_name", "equal", "Isaac")}),
+                PaginatedFilter({"condition_tree": ConditionTreeLeaf("author:first_name", "in", ["Isaac", "J.K."])}),
                 Projection("id", "author_id"),
             )
+
+    def test_should_disable_join_on_condition_tree_but_not_in_projection(self):
+        with patch.object(
+            self.collection_book,
+            "list",
+            new_callable=AsyncMock,
+            return_value=[
+                {"id": 1, "author": {"first_name": "Isaac"}},
+                {"id": 2, "author": {"first_name": "J.K."}},
+                {"id": 3, "author": {"first_name": "J.K."}},
+            ],
+        ) as mock_list:
+            response = self.loop.run_until_complete(
+                self.decorated_book_collection.list(
+                    self.mocked_caller,
+                    PaginatedFilter({"condition_tree": ConditionTreeLeaf("author:id", "in", [2, 5])}),
+                    Projection("id", "author:first_name"),
+                )
+            )
+            mock_list.assert_awaited_once_with(
+                self.mocked_caller,
+                PaginatedFilter({"condition_tree": ConditionTreeLeaf("author_id", "in", [2, 5])}),
+                Projection("id", "author:first_name"),
+            )
+        self.assertEqual(
+            [
+                {"id": 1, "author": {"first_name": "Isaac"}},
+                {"id": 2, "author": {"first_name": "J.K."}},
+                {"id": 3, "author": {"first_name": "J.K."}},
+            ],
+            response,
+        )
+
+    def test_should_disable_join_on_projection_but_not_in_condition_tree(self):
+        with patch.object(
+            self.collection_book,
+            "list",
+            new_callable=AsyncMock,
+            return_value=[
+                {"id": 1, "author_id": 2},
+                {"id": 2, "author_id": 5},
+                {"id": 3, "author_id": 5},
+            ],
+        ) as mock_list:
+            response = self.loop.run_until_complete(
+                self.decorated_book_collection.list(
+                    self.mocked_caller,
+                    PaginatedFilter(
+                        {"condition_tree": ConditionTreeLeaf("author:first_name", "in", ["Isaac", "J.K."])}
+                    ),
+                    Projection("id", "author:id"),
+                )
+            )
+            mock_list.assert_awaited_once_with(
+                self.mocked_caller,
+                PaginatedFilter({"condition_tree": ConditionTreeLeaf("author:first_name", "in", ["Isaac", "J.K."])}),
+                Projection("id", "author_id"),
+            )
+        self.assertEqual(
+            [
+                {"id": 1, "author": {"id": 2}},
+                {"id": 2, "author": {"id": 5}},
+                {"id": 3, "author": {"id": 5}},
+            ],
+            response,
+        )
