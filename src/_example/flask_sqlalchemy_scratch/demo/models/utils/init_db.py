4d3a87528d806e8ac4184081cefd4b7b1c18dce7
Julien Barreau <julien.barreau@forestadmin.com>
2024-12-27 16:19:55 +0100

    chore: remove useless example project

--- src/_example/flask_sqlalchemy_scratch/demo/models/utils/init_db.py
+++
@@ -1,8 +0,0 @@
-from demo.models.models import DB_URI, Base
-from sqlalchemy import create_engine
-
-
-def create_all():
-    engine = create_engine(DB_URI, echo=False)
-    with engine.begin() as conn:
-        Base.metadata.create_all(conn)
