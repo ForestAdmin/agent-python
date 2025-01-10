18dc451069667413b8f43495deb4a127845e2d04
Julien Barreau <julien.barreau@forestadmin.com>
2025-01-10 14:54:03 +0100

    chore: improve typing for mcp tools schema

--- src/agent_toolkit/forestadmin/agent_toolkit/resources/mcp.py
+++ src/agent_toolkit/forestadmin/agent_toolkit/resources/mcp.py
@@ -6,7 +6,7 @@ import logging
 import os
 from multiprocessing import Condition
 from threading import Thread
-from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union, cast
+from typing import TYPE_CHECKING, Annotated, Any, Dict, List, Optional, Union, cast
 
 from forestadmin.agent_toolkit.forest_logger import ForestLogger
 from forestadmin.agent_toolkit.options import Options
@@ -29,14 +29,17 @@ from forestadmin.datasource_toolkit.datasources import Datasource
 from forestadmin.datasource_toolkit.exceptions import ForbiddenError
 from forestadmin.datasource_toolkit.interfaces.fields import is_column
 from forestadmin.datasource_toolkit.interfaces.query.condition_tree.factory import ConditionTreeFactory
-from forestadmin.datasource_toolkit.interfaces.query.condition_tree.nodes.leaf import ConditionTreeLeaf
+from forestadmin.datasource_toolkit.interfaces.query.condition_tree.nodes.branch import BranchComponents
+from forestadmin.datasource_toolkit.interfaces.query.condition_tree.nodes.leaf import ConditionTreeLeaf, LeafComponents
 from forestadmin.datasource_toolkit.interfaces.query.filter.paginated import PaginatedFilter
 from forestadmin.datasource_toolkit.interfaces.query.filter.unpaginated import Filter
 from forestadmin.datasource_toolkit.interfaces.query.projections.factory import ProjectionFactory
 from forestadmin.datasource_toolkit.utils.schema import SchemaUtils
+from forestadmin.datasource_toolkit.validations.projection import ProjectionValidator
 from mcp import types
 from mcp.server.fastmcp import Context, FastMCP
 from mcp.server.fastmcp.server import Settings
+from pydantic import Field
 
 if TYPE_CHECKING:
     from forestadmin.agent_toolkit.agent import Agent
@@ -109,33 +112,46 @@ class MCPResource(Thread):
             return SchemaEmitter.serialize(collection_schema, self.agent.meta)
 
     def setup_tools(self):
+
         self.setup_metadata_tools()
         self.setup_data_access_tools()
         self.setup_actions_tools()
-        pass
+
+        # @self.mcp._mcp_server.list_tools()
+        # async def list_tools() -> List[types.Tool]:
+        #     # help debug schema
+        #     tools = await self.mcp.list_tools()
+        #     list_record_tool = [t for t in tools if t.name == "list-collection-records"][0]
+        #     return tools
 
     def setup_metadata_tools(self):
         @self.mcp.tool("list_collections")
-        def get_collections(any) -> List[str]:
+        def get_collections() -> List[str]:
             """list all the collections names known by forestadmin"""
             ret = [c.name for c in self.datasource.collections]
             ForestLogger.log("info", f"-- TOOL get_collections, {ret}")
             return ret
 
         @self.mcp.tool("collection-fields")
-        def get_fields(collection_name: str) -> List[str]:
+        def get_fields(
+            collection_name: Annotated[str, Field(description="the name of the collection you want the fields")]
+        ) -> List[str]:
             """list all fields and relations of collection {collection_name}"""
             ForestLogger.log("info", f"-- TOOL get_fields of {collection_name}")
             return self.datasource.get_collection(collection_name).schema["fields"]
 
         @self.mcp.tool("collection-segments")
-        def get_segments(collection_name: str) -> List[str]:
+        def get_segments(
+            collection_name: Annotated[str, Field(description="the name of the collection you want the segments")]
+        ) -> List[str]:
             """list all segments of collection {collection_name}"""
             ForestLogger.log("info", f"-- TOOL get_segments of {collection_name}")
             return self.datasource.get_collection(collection_name).schema["segments"]
 
         @self.mcp.tool("collection-actions")
-        def get_actions(collection_name: str) -> List[str]:
+        def get_actions(
+            collection_name: Annotated[str, Field(description="the name of the collection you want the actions")]
+        ) -> List[str]:
             """
             list all the actions of the collection {collection_name}.
             If there is only one field named 'Loading...' in the form, it means that the form is dynamic and you should
@@ -147,7 +163,14 @@ class MCPResource(Thread):
 
     def setup_actions_tools(self):
         @self.mcp.tool("collection-load-form")
-        async def load_form(collection_name: str, action_name: str) -> List[str]:
+        async def load_form(
+            collection_name: Annotated[
+                str, Field(description="the name of the collection you want the the form for action {action_name}")
+            ],
+            action_name: Annotated[
+                str, Field(description="the name of the action in collection {collection_name} you want the form")
+            ],
+        ) -> List[str]:
             """load form of action {action_name} of collection {collection_name}."""
             ForestLogger.log("info", f"-- TOOL load form of action {action_name} of collection {collection_name}")
             try:
@@ -162,41 +185,66 @@ class MCPResource(Thread):
 
         @self.mcp.tool("collection-execute-action")
         async def execute_action(
-            collection_name: str,
-            action_name: str,
-            record_id: Optional[Union[Any, List[Any]]] = None,
-            form: Optional[Any] = None,
+            collection_name: Annotated[
+                str, Field(description="the name of the collection you want execute {action_name} action")
+            ],
+            action_name: Annotated[
+                str, Field(description="the name of the action you want to execute on collection {collection_name}")
+            ],
+            record_id: Annotated[
+                Optional[Union[Any, List[Any]]],
+                Field(
+                    description="the record id to execute the action if action scope is 'Single', "
+                    "when action scope is 'bulk', this parameter should be a list of record ids, "
+                    "and when action scope is global, this parameter should be None",
+                    default=None,
+                ),
+            ],
+            form: Annotated[
+                Optional[Dict[str, Any]],
+                Field(
+                    description="when the action provides a form, the form to fill for the action. "
+                    "All fields with 'is_required=true must be filled by the end user.",
+                    examples=[{"label": "value"}],
+                    default=None,
+                ),
+            ],
         ) -> List[str]:
             """execute action {action_name} of collection {collection_name}, with form {form}.
             Actions can be found using collection-actions tool.
-            If there is a form, it must be filled, and given in {form}, and {form} must be formatted as: '{"label": "value"}'.
+            If there is a form, it must be filled, and given in {form}, and {form} must be
+            formatted as:'{"label": "value"}'.
             Be sure the user verified the form before calling this tool.
             """
             ForestLogger.log(
                 "info",
                 f"-- TOOL execute of action {action_name} of collection {collection_name}. form is {form}. record id is {record_id}",
             )
-            if action_name not in self.datasource.get_collection(collection_name).schema["actions"]:
+            collection = self.datasource.get_collection(collection_name)
+            if action_name not in collection.schema["actions"]:
                 raise Exception(
                     f"Action {action_name} not found in collection {collection_name}, "
-                    f"actions are {', '.join(self.datasource.get_collection(collection_name).schema['actions'].keys())}"
+                    f"actions are {', '.join(collection.schema['actions'].keys())}"
                 )
+            action = collection.schema["actions"][action_name]
+            if action.scope.value == "Single" and (not record_id or isinstance(record_id, list)):
+                raise Exception("record id must be an unique id")
+            if action.scope.value == "Bulk" and (not record_id or not isinstance(record_id, list)):
+                raise Exception("record id must be a list of ids")
             form = form or {}
             filter_ = Filter({})
             if record_id:
                 filter_ = filter_.override(
                     {
                         "condition_tree": ConditionTreeLeaf(
-                            SchemaUtils.get_primary_keys(self.datasource.get_collection(collection_name).schema)[0],
+                            SchemaUtils.get_primary_keys(collection.schema)[0],
                             "in",
                             record_id if isinstance(record_id, list) else [record_id],
                         )
                     }
                 )
             try:
-                ret = await cast(Collection, self.datasource.get_collection(collection_name)).execute(
-                    None, action_name, form, filter_
-                )
+                ret = await cast(Collection, collection).execute(None, action_name, form, filter_)
                 ForestLogger.log("info", f"-- TOOL execute of action of {collection_name}: {str(ret)} ")
                 return ret
             except Exception as exc:
@@ -205,57 +253,72 @@ class MCPResource(Thread):
 
     def setup_data_access_tools(self):
         @self.mcp.tool("list-collection-records")
-        async def list(collection_name: str) -> List[Dict[str, Any]]:
+        async def list(
+            collection_name: Annotated[
+                str,
+                Field(
+                    description="the collection name to get records from",
+                ),
+            ],
+            condition_tree: Annotated[
+                Optional[Union[LeafComponents, BranchComponents]],
+                Field(
+                    description="condition tree to filter in the collection",
+                    default=None,
+                ),
+            ],
+            search: Annotated[
+                Optional[str],
+                Field(
+                    description="a text to search over searchable fields. Use None to not search",
+                    default=None,
+                ),
+            ],
+            search_extended: Annotated[
+                Optional[bool],
+                Field(
+                    description="when a {search} is given, set this parameter to true to search over relations, "
+                    "otherwise it will just search in collection columns",
+                    default=None,
+                ),
+            ],
+            projection: Annotated[
+                Optional[List[str]],
+                Field(
+                    description="the list of field to return for each record. ':' is used to pass through relations. "
+                    "If set to None, all records will be used. Projection must not contains relations",
+                    default=None,
+                ),
+            ],
+            # page: PlainPage
+        ) -> List[Dict[str, Any]]:
             """list records of {collection_name}. collection_name can be found using get_collections tool"""
-            ForestLogger.log("info", f"-- TOOL get_list of {collection_name}")
-            ret = await self.datasource.get_collection(collection_name).list(
-                None,
-                PaginatedFilter({}),
-                ProjectionFactory.all(self.datasource.get_collection(collection_name)),
-            )
-            ForestLogger.log("info", f"-- TOOL get_list of {collection_name} ; {len(ret)} records")
-            # await ctx.report_progress(100, 100)
-            return ret
+            try:
+                collection = self.datasource.get_collection(collection_name)
+                ForestLogger.log("info", f"-- TOOL get_list of {collection_name}")
+                filter_ = PaginatedFilter({})
+                if condition_tree:
+                    parsed_condition_tree = ConditionTreeFactory.from_plain_object(condition_tree)
+                    ProjectionValidator.validate(collection, parsed_condition_tree.projection)
 
-        @self.mcp.tool("search-in-collection")
-        async def search(collection_name: str, search: Union[int, float, str, Dict[str, Any]]) -> List[Dict[str, Any]]:
-            """
-            search in collection {collection_name} for entry matching {search}.
-            collection_name can be found using get_collections tool
-            """
-            ForestLogger.log("info", f"-- TOOL search({search}) of {collection_name}")
-            filter_ = PaginatedFilter({})
-            schema = self.datasource.get_collection(collection_name).schema
-            if isinstance(search, dict):
-                available_fields = [field for field in schema["fields"].keys() if is_column(schema["fields"][field])]
-                for key, value in search.items():
-                    if key not in schema["fields"]:
-                        raise Exception(
-                            f"Field {key} not found in collection {collection_name}, fields are {', '.join(available_fields)}"
-                        )
-                    if not is_column(schema["fields"][key]):
-                        raise Exception(
-                            f"Field {key} is a relation in collection {collection_name}, you must search over columns: "
-                            f"{', '.join(available_fields)}"
-                        )
+                    filter_ = filter_.override({"condition_tree": parsed_condition_tree})
+                if search and len(search) > 0:
+                    filter_ = filter_.override({"search": search, "search_extended": search_extended or False})
 
-                filter_ = filter_.override(
-                    {
-                        "condition_tree": ConditionTreeFactory.intersect(
-                            [ConditionTreeLeaf(key, "equal", value) for key, value in search.items()]
-                        )
-                    }
-                )
-            else:
-                filter_ = filter_.override({"search": str(search)})
+                if projection:
+                    ProjectionValidator.validate(collection, projection)
+
+            except Exception as exc:
+                return str(exc)
 
             try:
-                ret = await self.datasource.get_collection(collection_name).list(
+                ret = await collection.list(
                     None,
                     filter_,
-                    ProjectionFactory.all(self.datasource.get_collection(collection_name)),
+                    (ProjectionFactory(*projection) if projection else ProjectionFactory.all(collection)),
                 )
-                ForestLogger.log("info", f"-- TOOL search({search}) of {collection_name} ; {len(ret)} records")
+                ForestLogger.log("info", f"-- TOOL get_list of {collection_name} ; {len(ret)} records")
                 return ret
             except Exception as exc:
                 ForestLogger.log("exception", f"mcp execute error {exc}")
+                return str(exc)
