78f7eb33e7ce59389336c52b85bb415911eb1f67
Julien Barreau <julien.barreau@forestadmin.com>
2025-02-05 15:53:13 +0100

    chore: adapt test on crud related

--- src/agent_toolkit/forestadmin/agent_toolkit/resources/collections/crud_related.py
+++ src/agent_toolkit/forestadmin/agent_toolkit/resources/collections/crud_related.py
@@ -27,11 +27,9 @@ from forestadmin.agent_toolkit.resources.collections.requests import (
     RequestCollectionException,
     RequestRelationCollection,
 )
-from forestadmin.agent_toolkit.services.serializers import DumpedResult, add_search_metadata
-from forestadmin.agent_toolkit.services.serializers.json_api import JsonApiException, JsonApiSerializer
-from forestadmin.agent_toolkit.services.serializers.json_api_serializer import (
-    JsonApiSerializer as JsonApiSerializerHomeMade,
-)
+from forestadmin.agent_toolkit.services.serializers import add_search_metadata
+from forestadmin.agent_toolkit.services.serializers.exceptions import JsonApiException
+from forestadmin.agent_toolkit.services.serializers.json_api_serializer import JsonApiSerializer
 from forestadmin.agent_toolkit.utils.context import HttpResponseBuilder, Request, RequestMethod, Response
 from forestadmin.agent_toolkit.utils.csv import Csv, CsvException
 from forestadmin.agent_toolkit.utils.id import unpack_id
@@ -107,9 +105,10 @@ class CrudRelatedResource(BaseCollectionResource):
             paginated_filter,
             projection,
         )
-        schema = JsonApiSerializer.get(request.foreign_collection)
+        # schema = JsonApiSerializerOld.get(request.foreign_collection)
         try:
-            dumped: DumpedResult = schema(projections=projection).dump(records, many=True)  # type: ignore
+            dumped = JsonApiSerializer(self.datasource, projection).serialize(records, request.foreign_collection)
+        #     dumped: DumpedResult = schema(projections=projection).dump(records, many=True)  # type: ignore
         except JsonApiException as e:
             ForestLogger.log("exception", e)
             return HttpResponseBuilder.build_client_error_response([e])
@@ -117,24 +116,19 @@ class CrudRelatedResource(BaseCollectionResource):
         if paginated_filter.search:
             dumped = add_search_metadata(dumped, paginated_filter.search)
 
-        try:
-            new_ret = JsonApiSerializerHomeMade(self.datasource, projection).serialize(
-                records, request.foreign_collection
-            )
-            if paginated_filter.search:
-                dumped = add_search_metadata(dumped, paginated_filter.search)
-            from dictdiffer import diff as differ
+        # try:
+        #     if paginated_filter.search:
+        #         dumped = add_search_metadata(dumped, paginated_filter.search)
+        #     # from dictdiffer import diff as differ
 
-            diff = list(differ(dumped, new_ret))
-            ForestLogger.log("info", f"returning new_ret({request.foreign_collection.name}) ... diff({len(diff)})")
-            if len(diff) > 0:
-                pass
-            return HttpResponseBuilder.build_success_response(cast(Dict[str, Any], dumped))
-        except Exception as exc:
-            traceback.print_exc()
-            pass
-            # raise
-
+        #     # diff = list(differ(dumped, new_ret))
+        #     # ForestLogger.log("info", f"returning new_ret({request.foreign_collection.name}) ... diff({len(diff)})")
+        #     # if len(diff) > 0:
+        #     #     pass
+        #     return HttpResponseBuilder.build_success_response(cast(Dict[str, Any], dumped))
+        # except Exception:
+        #     traceback.print_exc()
+        #     raise
         return HttpResponseBuilder.build_success_response(cast(Dict[str, Any], dumped))
 
     @authenticate
