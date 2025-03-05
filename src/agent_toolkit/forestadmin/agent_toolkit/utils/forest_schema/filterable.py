f5c960052f11792c93160ca19570338311ddd10c
Julien Barreau <julien.barreau@forestadmin.com>
2025-03-05 17:48:00 +0100

    chore: fix formatting

--- src/agent_toolkit/forestadmin/agent_toolkit/utils/forest_schema/filterable.py
+++ src/agent_toolkit/forestadmin/agent_toolkit/utils/forest_schema/filterable.py
@@ -4,7 +4,6 @@ from forestadmin.datasource_toolkit.interfaces.fields import Operator
 
 
 class FrontendFilterableUtils:
-
     @classmethod
     def is_filterable(cls, operators: Set[Operator]) -> bool:
         return operators is not None and len(operators) > 0
