036a139164e38d8299cd23c85d9d7d8795400386
Julien Barreau <julien.barreau@forestadmin.com>
2025-03-14 11:08:02 +0100

    fix(validation): validation rules were duplicated in schema

--- src/datasource_toolkit/forestadmin/datasource_toolkit/decorators/validation/collection.py
+++ src/datasource_toolkit/forestadmin/datasource_toolkit/decorators/validation/collection.py
@@ -52,9 +52,7 @@ class ValidationCollectionDecorator(CollectionDecorator):
     def _refine_schema(self, sub_schema: CollectionSchema) -> CollectionSchema:
         fields_schema = {**sub_schema["fields"]}
         for name, rules in self.validations.items():
-            if fields_schema[name].get("validations") is None:
-                fields_schema[name]["validations"] = []
-            fields_schema[name]["validations"].extend(rules)
+            fields_schema[name]["validations"] = [*fields_schema[name].get("validations", []), *rules]
 
         return {**sub_schema, "fields": fields_schema}
 
