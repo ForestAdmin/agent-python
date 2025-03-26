696837534a57bd6924d544838b84111319078a99
Julien Barreau <julien.barreau@forestadmin.com>
2025-03-26 16:44:53 +0100

    chore: simplify the reloading

--- src/datasource_rpc/forestadmin/datasource_rpc/datasource.py
+++ src/datasource_rpc/forestadmin/datasource_rpc/datasource.py
@@ -1,4 +1,5 @@
 import asyncio
+import hashlib
 import json
 import os
 import signal
@@ -8,50 +9,58 @@ from typing import Any, Dict
 
 import grpc
 import urllib3
+from forestadmin.agent_toolkit.forest_logger import ForestLogger
 from forestadmin.agent_toolkit.utils.context import User
 from forestadmin.datasource_rpc.collection import RPCCollection
-from forestadmin.datasource_rpc.reloadable_datasource import ReloadableDatasource
 from forestadmin.datasource_toolkit.datasources import Datasource
-from forestadmin.datasource_toolkit.decorators.action.collections import ActionCollectionDecorator
-from forestadmin.datasource_toolkit.interfaces.actions import Action, ActionsScope
 from forestadmin.datasource_toolkit.interfaces.chart import Chart
+from forestadmin.datasource_toolkit.utils.user_callable import call_user_function
 from forestadmin.rpc_common.hmac import generate_hmac
 from forestadmin.rpc_common.serializers.aes import aes_decrypt, aes_encrypt
 from forestadmin.rpc_common.serializers.schema.schema import SchemaDeserializer
 from forestadmin.rpc_common.serializers.utils import CallerSerializer
 from sseclient import SSEClient
 
-# from forestadmin.rpc_common.proto import datasource_pb2_grpc
-# from google.protobuf import empty_pb2
+
+def hash_schema(schema) -> str:
+    return hashlib.sha256(json.dumps(schema, sort_keys=True).encode()).hexdigest()
 
 
-class RPCDatasource(Datasource, ReloadableDatasource):
-    def __init__(self, connection_uri: str, secret_key: str):
+class RPCDatasource(Datasource):
+    def __init__(self, connection_uri: str, secret_key: str, reload_method=None):
         super().__init__([])
         self.connection_uri = connection_uri
+        self.reload_method = reload_method
         self.secret_key = secret_key
         self.aes_key = secret_key[:16].encode()
         self.aes_iv = secret_key[-16:].encode()
-        # res = asyncio.run(self.connect_sse())
-        # Timer(5, self.internal_reload).start()
-        # self.trigger_reload()
+        self.last_schema_hash = ""
+
         self.http = urllib3.PoolManager()
-        self.wait_for_reconnect()
+        self.wait_for_connection()
         self.introspect()
-        self.thread = Thread(target=self.run, name="RPCDatasourceSSEThread", daemon=True)
+
+        self.thread = Thread(target=asyncio.run, args=(self.run(),), name="RPCDatasourceSSEThread", daemon=True)
         self.thread.start()
 
-    def introspect(self):
+    def introspect(self) -> bool:
+        """return true if schema has changed"""
+        response = self.http.request("GET", f"http://{self.connection_uri}/schema")
+        schema_data = json.loads(response.data.decode("utf-8"))
+        if self.last_schema_hash == hash_schema(schema_data):
+            ForestLogger.log("debug", "[RPCDatasource] Schema has not changed")
+            return False
+        self.last_schema_hash = hash_schema(schema_data)
+
         self._collections = {}
         self._schema = {"charts": {}}
-        response = self.http.request("GET", f"http://{self.connection_uri}/schema")
-        # schema_data = json.loads(response.data.decode("utf-8"))
-        schema_data = SchemaDeserializer().deserialize(response.data.decode("utf-8"))
+        schema_data = SchemaDeserializer().deserialize(schema_data)
         for collection_name, collection in schema_data["collections"].items():
             self.create_collection(collection_name, collection)
 
         self._schema["charts"] = {name: None for name in schema_data["charts"]}
         self._live_query_connections = schema_data["live_query_connections"]
+        return True
 
     def create_collection(self, collection_name, collection_schema):
         collection = RPCCollection(collection_name, self, self.connection_uri, self.secret_key)
@@ -59,7 +68,6 @@ class RPCDatasource(Datasource, ReloadableDatasource):
             collection.add_field(name, field)
 
         if len(collection_schema["actions"]) > 0:
-            # collection = ActionCollectionDecorator(collection, self)
             for action_name, action_schema in collection_schema["actions"].items():
                 collection.add_rpc_action(action_name, action_schema)
 
@@ -67,59 +75,36 @@ class RPCDatasource(Datasource, ReloadableDatasource):
 
         self.add_collection(collection)
 
-    def run(self):
-        self.wait_for_reconnect()
+    def wait_for_connection(self):
+        while True:
+            try:
+                self.http.request("GET", f"http://{self.connection_uri}/")
+                break
+            except Exception:
+                time.sleep(1)
+        ForestLogger.log("debug", "Connection to RPC datasource established")
+
+    async def internal_reload(self):
+        has_changed = self.introspect()
+        if has_changed and self.reload_method is not None:
+            await call_user_function(self.reload_method)
+
+    async def run(self):
+        self.wait_for_connection()
         self.sse_client = SSEClient(
             self.http.request("GET", f"http://{self.connection_uri}/sse", preload_content=False)
         )
         try:
             for msg in self.sse_client.events():
-                if msg.event == "heartbeat":
-                    continue
-
-                if msg.event == "RpcServerStop":
-                    print("RpcServerStop")
-                    break
-        except:
+                pass
+        except Exception:
             pass
-        print("rpc connection to server closed")
-        self.wait_for_reconnect()
-        self.internal_reload()
+        ForestLogger.log("info", "rpc connection to server closed")
+        self.wait_for_connection()
+        await self.internal_reload()
 
-        self.thread = Thread(target=self.run, name="RPCDatasourceSSEThread", daemon=True)
+        self.thread = Thread(target=asyncio.run, args=(self.run(),), name="RPCDatasourceSSEThread", daemon=True)
         self.thread.start()
-
-        # self.channel = grpc.aio.insecure_channel(self.connection_uri)
-        # async with grpc.aio.insecure_channel(self.connection_uri) as channel:
-        # stub = datasource_pb2_grpc.DataSourceStub(self.channel)
-        # response = await stub.Schema(empty_pb2.Empty())
-        # self.channel
-
-        return
-
-    def wait_for_reconnect(self):
-        while True:
-            try:
-                self.ping_connect()
-                break
-            except:
-                time.sleep(1)
-        print("reconntected")
-
-    def connect(self):
-        self.ping_connect()
-
-    def ping_connect(self):
-        self.http.request("GET", f"http://{self.connection_uri}/")
-
-    def internal_reload(self):
-        # Timer(5, self.internal_reload).start()
-        print("trigger reload")
-        self.introspect()
-        self.trigger_reload()
-
-    # def reload_agent(self):
-    #     os.kill(os.getpid(), signal.SIGUSR1)
 
     async def execute_native_query(self, connection_name: str, native_query: str, parameters: Dict[str, str]) -> Any:
         body = aes_encrypt(
